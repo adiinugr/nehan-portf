7eac52e5a6b8662047eb9f3b8601a0680829684c
Adi Nugroho <addieze@ymail.com>
2026-06-05 08:23:45 +0700

    fix

--- .next/static/chunks/app_layout_tsx_f0e4c1a2._.js
+++ .next/static/chunks/app_layout_tsx_f0e4c1a2._.js
@@ -5,7 +5,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/app_layout_tsx_f0e4c1a2._.js",
   "chunks": [
-    "static/chunks/[root of the server]__22e704b9._.css",
+    "static/chunks/[root of the server]__ba0bcd9f._.css",
     "static/chunks/node_modules_205d50fa._.js",
     "static/chunks/_64fdae4a._.js"
   ],
