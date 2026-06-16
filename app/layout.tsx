4311c6224a723d72e462c672c8d05b0f2cf1699f
Adi Nugroho <addieze@ymail.com>
2026-06-16 11:47:27 +0700

    add ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -128,6 +128,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <WhatsAppButton />
         </ThemeProvider>
 
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+              `}
+            </Script>
+          </>
+        )}
+
         <Script id="schema-structured-data" type="application/ld+json" strategy="afterInteractive">
           {`
             {
